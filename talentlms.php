b5332da814fb05948225e94f93ac94ed7f907a45
V <V.@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-03-20 10:01:27 +0000

    - new version of TalentLMSLIb
    git-svn-id: https://plugins.svn.wordpress.org/talentlms/trunk@684683 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- talentlms.php
+++ talentlms.php
@@ -3,13 +3,13 @@
  Plugin Name: TalentLMS
  Plugin URI: http://wordpress.org/extend/plugins/talentlms/
  Description: This plugin integrates Talentlms with Wordpress. Promote your TalentLMS content through your WordPress site.
- Version: 2.2.2
+ Version: 2.2.3
  Author: Vasilis Proutzos / Epignosis LTD
  Author URI: www.talentlms.com
  License: GPL2
  */
 
-define("_VERSION_", "2.2.2");
+define("_VERSION_", "2.2.3");
 define("_BASEPATH_", dirname(__FILE__));
 define("_BASEURL_", plugin_dir_url(__FILE__));
 
