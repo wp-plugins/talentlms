cd40a1a262f9499f1c19f285013ac4cbd1b0ab2d
V <V.@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-07-07 13:24:31 +0000

    delete everything
    git-svn-id: https://plugins.svn.wordpress.org/talentlms/trunk@1194085 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- TalentLMSLib/lib/TalentLMS/SiteInfo.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-class TalentLMS_Siteinfo extends TalentLMS_ApiResource{
-	
-	public static function get(){
-		$class = get_class();
-		return self::_scopedAll($class);
-	}
-}
--- TalentLMSLib/lib/TalentLMS/Unit.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-class TalentLMS_Unit extends TalentLMS_ApiResource{
-	
-	public static function getUsersProgress($params){
-		$class = get_class();
-		return self::_scopedGetUsersProgressInUnits($class, $params);
-	}
-}
